555cddaf800e99da68e1fe3fcb89ae4a058578b9
MaZhen <1033516561@qq.com>
2018-08-11 20:42:20 +0800

    eureka测试

---
+++ eureka/src/main/java/com/dhu/eureka/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.dhu.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// TODO: 2018/8/11 1. 一开始直接启动新的项目没有反应，在此注释EnableEurekaServer
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
